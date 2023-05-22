c3941fc4a9c97062dd954c5bd9ddc0a4708904a3
Solo VV <makler63@yandex.ru>
2023-05-22 14:34:07 +0300

    ADD transferActionTest

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,9 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
 
@@ -16,5 +16,17 @@ public class DragAndDropTest {
         $("#column-a").dragAndDropTo("#column-b");
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    //Тест не работает
+    void transferActionTest () {
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browser = System.getProperty("browser", "firefox");
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").shouldHave(text("A"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b"));
+        $("#column-a").shouldHave(exactText("B"));
     }
 }
