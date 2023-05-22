002e9f8c35a7f4431ed7a1b0a1c404b19fa70a4d
Solo VV <makler63@yandex.ru>
2023-05-22 22:50:10 +0300

    ADD configProperty

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -6,13 +7,14 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
-
-    @Test
-    void transferTest () {
+    @BeforeAll
+    public static void configProperty () {
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.browser = System.getProperty("browser", "firefox");
         open("https://the-internet.herokuapp.com/drag_and_drop");
-
+    }
+    @Test
+    void transferTest () {
         $("#column-a").dragAndDropTo("#column-b");
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
@@ -21,10 +23,6 @@ public class DragAndDropTest {
     @Test
     //Тест не работает
     void transferActionTest () {
-        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
-        Configuration.browser = System.getProperty("browser", "firefox");
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-
         $("#column-a").shouldHave(text("A"));
         actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b"));
         $("#column-a").shouldHave(exactText("B"));
--- src/test/java/FindPageEnterpriseTest.java
+++ src/test/java/FindPageEnterpriseTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,13 +7,14 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FindPageEnterpriseTest {
-
-    @Test
-    void findEnterprise () {
+    @BeforeAll
+    public static void configProperty () {
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.browser = System.getProperty("browser", "firefox");
         open("https://github.com");
-
+    }
+    @Test
+    void findEnterprise () {
         $("div.header-menu-wrapper").$(byText("Solutions")).hover();
         $(byText("Enterprise")).click();
         $("div.enterprise-hero").shouldHave(text("Build like the best"));
